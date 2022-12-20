31a387038b69fb54f9753ff996661024ca1ba7b1
Shahmirze <sahmirze.memmedyarov@gmail.com>
2022-12-21 00:13:59 +0400

    remove add and edit create

--- edit_remove click/app.js
+++ edit_remove click/app.js
@@ -1,17 +1,37 @@
 const text = document.querySelector('.text')
-const remove = document.getElementById('remove')
-const edit = document.getElementById('edit')
+const remove = document.querySelector('.remove')
+const edit = document.querySelector('.edit')
 const container = document.querySelector('.container')
 const textAdd = document.getElementById('textAdd')
 const btn = document.querySelector('.btn')
+
+
+
 remove.onclick = () => {
     container.style.display = "none"
 }
-if (text.style.display !== "none") {
-    edit.onclick = () => {
+
+if (text.style.display != "none") {
+    edit.addEventListener('click', () => {
         let metn = prompt('hi make up friends please')
-        if (metn.length != 0) {
+        if (metn.length != null) {
             text.textContent = metn.replaceAll(":)", "😀")
         }
-    }
+    })
 }
+
+btn.addEventListener('click', () => {
+    let divCont = document.createElement('div')
+    divCont.classList.add('container')
+    let pText = document.createElement('p')
+    pText.textContent = " Lorem ipsum, dolor sit amet consectetur adipisicing elit. Recusandae commodi architecto, vitae eius similique magni a sapiente quo alias! Obcaecati dolore cum, soluta porro recusandae autem consequuntur! Animi, voluptatem incidunt"
+    pText.classList.add('text')
+    let btnRemove = document.createElement('button')
+    btnRemove.classList.add('remove')
+    btnRemove.textContent = '❌'
+    let btnEdit = document.createElement('button')
+    btnEdit.classList.add('edit')
+    btnEdit.textContent = '🔁'
+    document.body.appendChild(divCont)
+    divCont.append(pText, btnRemove, btnEdit)
+})
