185d1017e6d283d88f85afbc2178367a2c162aef
Shahmirze <sahmirze.memmedyarov@gmail.com>
2022-12-22 22:42:38 +0400

    I finished this project (remove and added).

--- edit_remove click/app.js
+++ edit_remove click/app.js
@@ -1,55 +1,53 @@
-const textAdd = document.getElementById('textAdd')
 const btn = document.querySelector('.btn')
+const textAdd = document.getElementById('textAdd')
 const keep = document.getElementById('keep')
-let metn;
 
-function call() {
+let x = 1
+let arr = [{
+    id: 1,
+    text: "salam"
+
+}]
+
+function Call() {
+    // $ { inc }
+    // $ { inc }
+    // $ { inc }
+    let inc = x++;
     let div = document.createElement('div')
-    div.classList.add('container')
+    div.className = `container`
+    div.setAttribute('id', `test${inc}`)
     let p = document.createElement('p')
-    p.classList.add('text')
-    p.textContent = "I should have gone to class, but I didn't"
+    p.className = `text `
+    p.textContent = "Somtimes you get fell inside and I want to kill it"
     let btnEdit = document.createElement('button')
-    btnEdit.classList.add('edit')
+    btnEdit.className = `edit`
     btnEdit.textContent = "🔁"
     let btnRemove = document.createElement('button')
-    btnRemove.classList.add('remove')
-    btnRemove.textContent = "❌"
+    btnRemove.className = `remove`
+    btnRemove.textContent = '❌'
     keep.append(div)
     div.append(p, btnEdit, btnRemove)
-}
-
-call()
-let x = 0
-btn.onclick = () => {
-    call()
-
-}
-
-const remove = document.querySelector('.remove')
-const edit = document.querySelector('.edit')
-const container = document.querySelector('.container')
-const text = document.querySelector('.text')
-remove.onclick = () => {
-    keep.children[x++].style.display = "none"
-
-    // console.log(container.len)
-}
-
-if (text.style.display != "none") {
-    edit.addEventListener('click', () => {
-        metn = prompt('hi make up friends please')
-        let colorRed = text.style.color = "red"
-
-        if (metn != null && metn != "") {
-            text.textContent = metn.replaceAll(":)", "😀")
-        } else if (metn == "") {
-            text.textContent = "bos niye saxlamisan mandalin 😡😡😡😡😡😡😡😡"
-            colorRed
+    let test = document.getElementById(`test${inc}`)
+        // console.log(test)
+    btnRemove.addEventListener('click', () => {
+        // test.style.display = "none"
+        test.remove()
+    })
+    btnEdit.addEventListener('click', () => {
+        let rewrite = prompt('hi, What do you want to write ')
+        console.log(rewrite)
+        if (rewrite == null) {
+            test.children[0].textContent = 'why 🤬'
+        } else if (rewrite != "") {
+            test.children[0].textContent = rewrite
 
         } else {
-            text.textContent = "🤬 why why 🤬🤬🤬🤬🤬🤬"
-            colorRed
+            test.children[0].textContent = 'your reason for keeping it blank'
+
         }
+
     })
+
 }
+btn.addEventListener('click', Call)
